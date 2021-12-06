4490ad70a5355abd3b0e258a8252a72a7ff51779
Victor Mello <66797422+VictorPMello@users.noreply.github.com>
2021-12-06 07:59:21 -0300

    Atualização de cores

--- js/form.js
+++ js/form.js
@@ -24,7 +24,7 @@ document.getElementById('submit').addEventListener('click', (e)=>{
       <span>Para as ${hora}</span>
       <span>no dia ${data}</span>
     </p>
-    <strong>Agradecimenos</strong>
+    <strong>Obrigado por consultar conosco</strong>
   </div>
   `;
 
